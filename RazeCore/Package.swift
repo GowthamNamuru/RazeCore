6ef96380e5246f9d521902ad7d8bc77575edc2f6
GowthamNamuri <gowthamnamuri@gmail.com>
2020-10-13 12:24:18 +0530

    Added comment for Carthage

--- RazeCore/Package.swift
+++ RazeCore/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RazeCore",
-            type: .dynamic,
+            type: .dynamic, // Added to make sure it works with Carthage.
             targets: ["RazeCore"]),
     ],
     dependencies: [
